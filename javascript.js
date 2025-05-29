52872c5ba44e6f987aae13473f63004d4859886f
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-05-29 14:24:57 +0530

    settimeout7closures

--- javascript.js
+++ javascript.js
@@ -26,14 +26,67 @@
     // console.log(a)
 
     // closures:
-    function x(){
-        var a =10;
-    function y(){
-         console.log(a)
-        }
-  a=100 //a is pointing to 100
-  return y;
-    }
-   var z = x();
-//    console.log(z);
-   z();
+//     function x(){
+//         var a =10;
+//     function y(){
+//          console.log(a)
+//         }
+//   a=100 //a is pointing to 100
+//   return y;
+//     }
+//    var z = x();
+// //    console.log(z);
+//    z();
+
+//    function x(){
+
+
+//     for(var i=1;i <=5; i++){
+//         function close(i){
+//         setTimeout(function () {
+//          console.log(i)
+//         },i*1000)
+//     }
+//     close (i);
+//     }
+//    }
+//    x()
+
+//    function outer(){
+//    var count=10;
+// return function inner(){
+// count++;
+// console.log(count)
+// }
+
+//    }
+//  outer()()
+
+
+// function counter(){
+//     let a =10;
+//     this.increement=function (){
+
+//         a++;
+//         console.log(a)
+//            }
+//     }
+
+
+// const counter1 =new counter;
+// counter1.increement();
+
+
+// functions:
+// function declaration or function statements:
+a();
+b();
+function a(){
+ console.log("hi")
+}
+ var b =function (){
+ console.log("bye")
+}
+
+
+
